cd2499c6f5e2bf4648f8265593f258fae91a2435
marcelo_dsa <marcelo_sousa@atlantico.com.br>
2025-08-07 14:10:45 -0300

    get and post endpoints

---
+++ API/server.js
@@ -0,0 +1,22 @@
+import express from 'express'
+
+const app = express()
+app.use(express.json())
+
+const users = []
+
+app.post('/users', (req, res) => {
+  const user = req.body
+  users.push(user)
+  res.status(201).send('User created')
+})
+
+app.get('/users', (req, res) => {
+  res.status(200).json(users)
+})
+
+app.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000')
+})
+
+export default app
