1633e04c01ccc66d30be2019d7920e2174fe5945
Marcelodsa <marcelo.egcomp@gmail.com>
2025-08-20 17:42:48 -0300

    showing users and creating

--- view/src/pages/home/index.jsx
+++ view/src/pages/home/index.jsx
@@ -1,19 +1,40 @@
-import { useEffect } from "react";
+import { useEffect, useState, useRef } from "react";
 import "./style.css";
 import Trash from "../../assets/trash.svg";
 import api from "../../services/api";
 
 function Home() {
-  let users = []
+  const [users, setUsers] = useState([]);
+
+  const inputName = useRef(null);
+  const inputAge = useRef(null);
+  const inputEmail = useRef(null);
 
   async function getUsers() {
     await api.get('/users')
       .then(response => {
-        users.push(...response.data);
+        setUsers(response.data);
       })
       .catch(error => {
         console.error("Error fetching users:", error);
       });
+  }
+
+  async function createUser() {
+    await api.post('/users', {
+      name: inputName.current.value,
+      age: parseInt(inputAge.current.value),
+      email: inputEmail.current.value
+    })
+    .then(() => {
+      getUsers();
+      inputName.current.value = '';
+      inputAge.current.value = '';
+      inputEmail.current.value = '';
+    })
+    .catch(error => {
+      console.error("Error creating user:", error);
+    });
   }
 
   useEffect(() => {
@@ -26,11 +47,11 @@ function Home() {
     <div className="container">
       <form>
         <h1>User Register</h1>
-        <input placeholder="Name" name="name" type="text" />
-        <input placeholder="Age" name="age" type="number" />
-        <input placeholder="Email" name="email" type="email" />
-        <button className="btn" type="button">
-          Submit
+        <input placeholder="Name" name="name" type="text" ref={inputName}/>
+        <input placeholder="Age" name="age" type="number" ref={inputAge}/>
+        <input placeholder="Email" name="email" type="email" ref={inputEmail}/>
+        <button className="btn" type="button" onClick={createUser}>
+          Create
         </button>
       </form>
 
